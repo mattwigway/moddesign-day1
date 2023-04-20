92b150e25ffdd893486b471fccb3fafc0f052779
Matt Bhagat-Conway <matt@indicatrix.org>
2023-04-20 09:20:52 -0400

    fix lib name

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(airlinestats2)
+library(airlinestats)
 
-test_check("airlinestats2")
+test_check("airlinestats")
